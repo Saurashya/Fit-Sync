1711454f91a557697aa73e1a121faa6b71c7396b
saurashya <saurashya1@gmail.com>
2024-08-31 20:50:56 +0545

    changes by saurashya

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex justify-center items-center h-screen">
       <Text className="text-red-600">Open up App.js to start working on your app!</Text>
       <Text className="text-blue-600">Changes you make will automatically reload.</Text>
       <StatusBar style="auto" />
@@ -11,11 +11,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
